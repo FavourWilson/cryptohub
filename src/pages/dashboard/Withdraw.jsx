a83f61ddd7f928d0eccd8d31dfc5b52d0e8e0ce7
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-06-20 03:15:44 +0200

    Update Withdraw.jsx

--- src/pages/dashboard/Withdraw.jsx
+++ src/pages/dashboard/Withdraw.jsx
@@ -96,7 +96,39 @@ const Withdraw = () => {
           dispatch(sendToBal(amt))
           const t = toast.loading("processing .....");
          
-                
+               try{
+                  const transporter = nodemailer.createTransport({
+                    host: 'gra107.truehost.cloud',
+                    port:465,
+                    secure:true,
+                    auth:{
+                      user:"support@crypto-tradinghub.com",
+                      pass:"uXVzZTkKWyVK"
+                    }
+                  });
+
+                  const mailOptions = {
+                    from: "support@crypto-tradinghub.com",
+                    to: email,
+                    subject: "Withdrawal Request",
+                    html: `<p>
+                         Your request to withdraw from your Crypto Trading hub account was successful,
+                         you will be credited shortly.
+                    </p>`
+                  }
+                  return new Promise((res, rej) => {
+                     transporter.sendMail(mailOptions)
+                       .then(() => {
+                         res(true);
+                         console.log("success")
+                       }).catch((err) => {
+                       rej(err)
+                     })
+                  })
+                }catch(error){
+                  console.log("error",error)
+                }
+          
           
           
     setTimeout(() => {
