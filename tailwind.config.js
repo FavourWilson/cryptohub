9d2f6cfac2408b49a4043e1286199bdf072f5f31
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-06-28 22:10:27 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -303,7 +303,7 @@ export default {
         fontFamily: {
           poppins: ["Poppins", "sans-serif"],
           dm: ["DM Sans", "sans-serif"],
-          Poliphilus:["Poliphilus"]
+          garmond:["Garmond"]
         },
         boxShadow: {
           "3xl": "14px 17px 40px 4px",
