efdd786431afd61fc325498d32610ccb9ea99c84
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-10-02 21:36:07 +0100

    Update Dashboard.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -29,74 +29,9 @@ const Dashboard = () => {
     <>
       <div className="max-w-full mx-auto py-3 lg:py-10 lg:pl-14">
 
-        {!isAdmin && (
-          <div className="flex flex-col-reverse gap-2 pb-5 items-start justify-start ">
-            <div className="w-full lg:w-[260px] rounded-r-sm px-3 py-1.5 lg:px-5 lg:py-2 space-y-1 flex flex-col justify-center items-center bg-yellow-500 border-l-4 border-l-main">
-              <p className="text-xs pt-2 font-medium capitalize text-center text-white">
-                Make deposit to start Investing
-              </p>
-              <Link
-                className="text-sm font-base w-[80%] lg:w-full capitalize text-center text-white rounded-lg hover:underline px-1 lg:px-3 py-1.5 bg-main dark:text-white dark:hover:text-white"
-                to="/dashboard/deposit"
-              >
-                {/* {brandText} */}
-                Click here to make a deposit
-              </Link>
-            </div>
-            <p className="shrink text-lg font-semibold capitalize text-navy-700 dark:text-white">
-              Welcome,{" "}
-              <span className="font-bold inline-block">{user.first_name}</span>!
-              <p className="block text-xs pt-2 font-base capitalize text-gray-500 dark:hover:text-white">
-                The most trusted bitcoin investment site.
-              </p>
-            </p>
-          </div>
-        )}
-        {isAdmin && (
-          <div className="bg-white px-4 py-4 rounded-lg mt-1 lg:mt-5 grid grid-cols-1 gap-5 lg:gap-2 md:grid-cols-2 lg:grid-cols-5 3xl:grid-cols-6">
-            <AllWidgets user={user} />
-            {/* <Widget
-            icon={<MdDashboard className="h-6 w-6" />}
-            title={"Your Balance"}
-            subtitle={"$1,000"}
-          />
-          <Widget
-            icon={<MdBarChart className="h-7 w-7" />}
-            title={"New Tasks"}
-            subtitle={"145"}
-          />
-          <Widget
-            icon={<IoMdHome className="h-6 w-6" />}
-            title={"Total Projects"}
-            subtitle={"$2433"}
-          /> */}
-          </div>
-        )}
-
-        <div className="space-y-5">
-          {!isAdmin && (
-            <div className="mt-5 grid grid-cols-1 gap-5 h-lg">
-              <TotalSpent />
-            </div>
-          )}
-          {!isAdmin && (
-            <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2 h-lg">
-                <TradeChart id="dfgh5d" coin="BTC" />
-                <TradeChart id="df5d3f" coin="ETH" />
-              </div>
-                )}
-              <div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
-              {isAdmin && (
-                <Tables
-                  show={!1}
-                  title="All Users"
-                  columnsData={userTransaction}
-                  tableData={allTransaction}
-                />
-                )}
-              <Activities />
-              </div>
-        </div>
+        {isAdmin ? 
+         <div className="bg-black text-white">gcfgcgjvjhgserstfgjk</div> :""
+        }
       </div>
     </>
   );
