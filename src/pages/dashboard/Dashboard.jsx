8af13db8b9f0bf1c3aaf19a17b3bbff27244be6f
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-10-02 21:14:58 +0100

    Update Dashboard.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
   return (
     <>
       <div className="max-w-full mx-auto py-3 lg:py-10 lg:pl-14">
-        {!!!isAdmin && (
+        {!isAdmin && (
           <div className="flex flex-col-reverse gap-2 pb-5 items-start justify-start ">
             <div className="w-full lg:w-[260px] rounded-r-sm px-3 py-1.5 lg:px-5 lg:py-2 space-y-1 flex flex-col justify-center items-center bg-yellow-500 border-l-4 border-l-main">
               <p className="text-xs pt-2 font-medium capitalize text-center text-white">
@@ -50,7 +50,7 @@ const Dashboard = () => {
             </p>
           </div>
         )}
-        {!!!isAdmin && (
+        {isAdmin && (
           <div className="bg-white px-4 py-4 rounded-lg mt-1 lg:mt-5 grid grid-cols-1 gap-5 lg:gap-2 md:grid-cols-2 lg:grid-cols-5 3xl:grid-cols-6">
             <AllWidgets user={user} />
             {/* <Widget
@@ -72,19 +72,19 @@ const Dashboard = () => {
         )}
 
         <div className="space-y-5">
-          {!!!isAdmin && (
+          {!isAdmin && (
             <div className="mt-5 grid grid-cols-1 gap-5 h-lg">
               <TotalSpent />
             </div>
           )}
-          {!!!isAdmin && (
+          {!isAdmin && (
             <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2 h-lg">
                 <TradeChart id="dfgh5d" coin="BTC" />
                 <TradeChart id="df5d3f" coin="ETH" />
               </div>
                 )}
               <div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
-              {!!!isAdmin && (
+              {isAdmin && (
                 <Tables
                   show={!1}
                   title="All Users"
