bd3307d409e4bc1d9087e06404a1863ec0131686
Ifedayo Prince Oni <ifedayoprince@gmail.com>
2023-10-03 14:04:14 +0100

    Update Dashboard.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -30,6 +30,11 @@ const Dashboard = () => {
       <div className="max-w-full mx-auto py-3 lg:py-10 lg:pl-14">
         <p className="">Hello World</p>
        <p>My name is {isAdmin}</p>
+        {(function(){
+            console.log("admin", isAdmin);
+            return <></>
+        })()
+        }
       </div>
     </>
   );
