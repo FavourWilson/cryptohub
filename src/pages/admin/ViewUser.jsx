84d5a1386de33d1a5b798fd901b1a25cf38fff10
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-10-06 02:24:10 +0100

    Update ViewUser.jsx

--- src/pages/admin/ViewUser.jsx
+++ src/pages/admin/ViewUser.jsx
@@ -3,11 +3,16 @@ import TransTable from '../../components/molecules/TransTable'
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import UserDetails from '../../components/organisms/UserDetails';
-
+import {useEffect} from 'react'
 const ViewUser = () => {
     const { admin } = useSelector((state) => state.user);
     const { user } = useParams();
     const data = admin.allUsers[user]
+
+    useEffect(()=>{
+
+        console.log(data);
+    },[])
   return (
     <>
         <div className='py-10'>
