9712feb35b468468a71caec9297e0935b2c97820
Ifedayo Prince Oni <ifedayoprince@gmail.com>
2023-10-07 19:17:41 +0100

    Update ViewUser.jsx

--- src/pages/admin/ViewUser.jsx
+++ src/pages/admin/ViewUser.jsx
@@ -8,14 +8,13 @@ const ViewUser = () => {
     const { admin } = useSelector((state) => state.user);
     const { user } = useParams();
     const data = admin.allUsers[user]
-
-    console.log("data", data)
+    
   return (
     <>
         <div className='py-10'>
         <Breadcrumb x={data.email} />
         <UserDetails data={data} />
-        <TransTable data={data.transactions}  />
+        <TransTable data={data.transaction}  />
       </div>
     </>
   );
