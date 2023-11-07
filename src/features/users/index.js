ef64d1ee018118f281a1ac703ca53e6af1af4f72
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-11-07 14:02:39 +0100

    Update index.js

--- src/features/users/index.js
+++ src/features/users/index.js
@@ -17,7 +17,7 @@ export const login = createAsyncThunk(
   "users/login",
   async ({ email, password }, thunkAPI) => {
     try {
-      let { data, status } = await Axios.get(`guser`, {
+      let { data, status } = await Axios.post(`guser`, {
         email,
       });
       if (status === 200) {
@@ -27,7 +27,7 @@ export const login = createAsyncThunk(
         };
 
         try {
-          let { data, status } = await Axios.get(`auth/`, body);
+          let { data, status } = await Axios.post(`auth/`, body);
           if (status === 200) {
             if(data.is_user_admin){
               localStorage.setItem("e70913ab-4047-48bc-8c33-aa2e7b3aeb2a", true)
