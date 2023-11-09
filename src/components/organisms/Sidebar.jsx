77c571989b67fc0cff509d89a4b018bc91961590
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-11-09 20:24:58 +0100

    Update Sidebar.jsx

--- src/components/organisms/Sidebar.jsx
+++ src/components/organisms/Sidebar.jsx
@@ -128,12 +128,7 @@ const Sidebar = ({ open, onClose }) => {
       {/* Nav item */}
 
       <ul className="mb-auto pt-1">
-        {!!!isAdmin ? 
-          <SidebarLinks adminLinks={adminLink} />
-          : 
-          <SidebarLinks routes={routes} />
-        
-        }
+        <SidebarLinks routes={routes} adminLinks={adminLink} />
       </ul>
 
       {/* Free Horizon Card
