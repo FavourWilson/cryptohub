f7ad8ea3fccc67688c533c7447a68de6b5d5f371
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-11-10 22:35:43 +0100

    Update index.js

--- src/features/users/index.js
+++ src/features/users/index.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import { getFaq } from "../landing";
 
 const token = () =>
-  localStorage.getItem("access_token");
+  localStorage.getItem("cfb90493-c364-4ade-820d-b6848bc65f44");
 
 const headers = {
   headers: {
