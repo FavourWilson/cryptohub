27768c70596c26cc8ea103b45e00d238ac8940fc
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-11-12 21:12:29 +0100

    Update TransTable.jsx

--- src/components/molecules/TransTable.jsx
+++ src/components/molecules/TransTable.jsx
@@ -11,8 +11,8 @@ const TransTable = ({ data }) => {
     ele.classList.toggle("hidden");
   };
 
-  const Approve = async (id) => {
-    const res = await dispatch(App({ id, type: "approve" }));
+  const Approve = async (uuid) => {
+    const res = await dispatch(App({ uuid, type: "approve" }));
     if (res.meta.requestStatus.toLowerCase() === "rejected") {
       // if (res.payload.statusText.toLowerCase() === "bad request") {
       // for (const prop in res?.payload?.detail) {
@@ -25,8 +25,6 @@ const TransTable = ({ data }) => {
       location.reload();
     }
   };
-
-  console.log("Transtable",data)
   return (
     <>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-3">
@@ -69,7 +67,7 @@ const TransTable = ({ data }) => {
             </tr>
           </thead>
           <tbody>
-            {data.map(x, i) => (
+            {data.map((x, i) => (
               <tr
                 key={`pp${i}`}
                 className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
@@ -78,7 +76,7 @@ const TransTable = ({ data }) => {
                   scope="row"
                   className="px-10 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  {x.id}
+                  {x.uuid}
                 </th>
                 <td className="px-10 py-4 text-gray-700">{x.plan}</td>
                 <td className="px-10 py-4 text-gray-700">${x.amount}</td>
@@ -105,7 +103,7 @@ const TransTable = ({ data }) => {
                     //   No Proof
                     // </button>
                     <button
-                      onClick={() => Popup(`p${x.id}`)}
+                      onClick={() => Popup(`p${x.uuid}`)}
                       data-modal-target="popup-modal"
                       data-modal-show="popup-modal"
                       className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -115,7 +113,7 @@ const TransTable = ({ data }) => {
                     </button>
                   ) : (
                     <button
-                      onClick={() => Popup(`p${x.id}`)}
+                      onClick={() => Popup(`p${x.uuid}`)}
                       data-modal-target="popup-modal"
                       data-modal-toggle="popup-modal"
                       className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -169,7 +167,7 @@ const TransTable = ({ data }) => {
                 <td className="px-10 py-4 text-right">
                   {x.status !== "funded" ? (
                     <span
-                      onClick={() => Popup(`p${x.id}`)}
+                      onClick={() => Popup(`p${x.uuid}`)}
                       className="cursor-pointer font-medium text-blue-600 dark:text-blue-500 hover:underline"
                     >
                       Edit
@@ -184,10 +182,10 @@ const TransTable = ({ data }) => {
         </table>
       </div>
 
-       {data.map((x, i) => (
+      {data.map((x, i) => (
         <div
           key={`po${i}`}
-          id={`p${x.id}`}
+          id={`p${x.uuid}`}
           data-modal-show="popup-modal"
           tabindex="-1"
           className={`${
@@ -197,7 +195,7 @@ const TransTable = ({ data }) => {
           <div className="relative w-full max-w-md max-h-full">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
               <button
-                onClick={() => Popup(`p${x.id}`)}
+                onClick={() => Popup(`p${x.uuid}`)}
                 type="button"
                 className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
                 data-modal-hide="popup-modal"
@@ -256,7 +254,7 @@ const TransTable = ({ data }) => {
                   x.status !== "funded" ? (
                     <>
                       <button
-                        onClick={() => Popup(`p${x.id}`)}
+                        onClick={() => Popup(`p${x.uuid}`)}
                         data-modal-hide="popup-modal"
                         type="button"
                         className="text-red-500 bg-white hover:bg-red-100 focus:ring-4 focus:outline-none focus:ring-red-200 rounded-lg border border-red-200 text-sm font-medium px-5 py-2.5 hover:text-red-900 focus:z-10 dark:bg-red-700 mr-2 dark:text-red-300 dark:border-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-600"
@@ -264,7 +262,7 @@ const TransTable = ({ data }) => {
                         No, cancel
                       </button>
                       <button
-                        onClick={() => Approve(x.id)}
+                        onClick={() => Approve(x.uuid)}
                         data-modal-hide="popup-modal"
                         type="button"
                         className="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
@@ -274,7 +272,7 @@ const TransTable = ({ data }) => {
                     </>
                   ) : (
                     <button
-                      onClick={() => Popup(`p${x.id}`)}
+                      onClick={() => Popup(`p${x.uuid}`)}
                       data-modal-hide="popup-modal"
                       type="button"
                       className="text-blue-500 bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-200 rounded-lg border border-blue-200 text-sm font-medium px-5 py-2.5 hover:text-blue-900 focus:z-10 dark:bg-blue-700 mr-2 dark:text-blue-300 dark:border-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-600"
@@ -285,8 +283,6 @@ const TransTable = ({ data }) => {
                 ) : (
                   ""
                 )}
-
-     
               </div>
             </div>
           </div>
