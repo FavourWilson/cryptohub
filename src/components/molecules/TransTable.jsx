86686637dd7257d8b614ef5c7b17209f8bca1d5d
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-11-12 17:11:07 +0100

    Update TransTable.jsx

--- src/components/molecules/TransTable.jsx
+++ src/components/molecules/TransTable.jsx
@@ -197,7 +197,7 @@ const TransTable = ({ data }) => {
           <div className="relative w-full max-w-md max-h-full">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
               <button
-                onClick={() => Popup(`p${x.uuid}`)}
+                onClick={() => Popup(`p${x.id}`)}
                 type="button"
                 className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
                 data-modal-hide="popup-modal"
