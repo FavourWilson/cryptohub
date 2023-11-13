4c72dccf27a99de3e5787311dd457cdb3079bfd1
Favour Wilson Ebrusike <41411068+FavourWilson@users.noreply.github.com>
2023-11-13 19:30:45 +0100

    Update index.js

--- src/features/users/index.js
+++ src/features/users/index.js
@@ -195,8 +195,7 @@ export const getUser = createAsyncThunk("app/me", async (_, thunkAPI) => {
 
 export const logout = createAsyncThunk("users/logout", async (_, thunkAPI) => {
   try {
-     localStorage.removeItem("cfb90493-c364-4ade-820d-b6848bc65f44");
-    localStorage.removeItem("e70913ab-4047-48bc-8c33-aa2e7b3aeb2a");
+    localStorage.removeItem("access_token");
     // localStorage.removeItem("refresh");
 
     const data = { message: "You have successfully Logged out", status: 200 };
